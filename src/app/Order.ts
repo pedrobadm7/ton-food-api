5a481c2e140759c147678c4c1adea7504699cda8
Pedro Barros <pedrobars7a@gmail.com>
2024-02-18 14:01:18 -0300

    feat: create routes

--- src/app/Order.ts
+++ src/app/Order.ts
@@ -24,7 +24,7 @@ export const Product = model('Product', new Schema({
       },
       quantity: {
         type: Number,
-        default: 1
+        default: 1,
       }
     }]
   }
---
+++ src/router.ts
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+
+export const router = Router();
+
+router.get('/categories', (req, res) => {
+  res.send('OK');
+});
+
+router.post('/categories', (req, res) => {
+  res.send('OK');
+});
+
+router.get('/products', (req, res) => {
+  res.send('OK');
+});
+
+router.post('/products', (req, res) => {
+  res.send('OK');
+});
+
+router.get('/categories/:categoryId/products', (req, res) => {
+  res.send('OK');
+});
+
+router.get('/orders', (req, res) => {
+  res.send('OK');
+});
+
+router.patch('/orders/:ordersId', (req, res) => {
+  res.send('OK');
+});
+
+router.delete('/orders/:ordersId', (req, res) => {
+  res.send('OK');
+});
