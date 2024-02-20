9edcc62cf7801ef401cb8883195095f1f636f592
Pedro Barros <pedrobars7a@gmail.com>
2024-02-20 00:18:11 -0300

    feature: envolve by try catch

--- src/app/useCases/categories/createCategory.ts
+++ src/app/useCases/categories/createCategory.ts
@@ -2,12 +2,16 @@ import { Request, Response } from 'express';
 import { Category } from '../../Category';
 
 export async function createCategories(req: Request, res: Response) {
-  const { icon, name } = req.body;
+  try {
+    const { icon, name } = req.body;
 
-  const category = await Category.create({
-    icon: icon,
-    name: name
-  });
+    const category = await Category.create({
+      icon: icon,
+      name: name
+    });
 
-  res.send(category);
+    res.status(201).json(category);
+  } catch {
+    res.status(500);
+  }
 }
